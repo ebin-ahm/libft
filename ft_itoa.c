3e1cff61ae33b43aa852e030b5ee4bc40186da33
Edieusdi Bin Ahmad <ebin-ahm@c2r2s7.42singapore.sg>
2025-11-29 21:03:21 +0800

    update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,8 +6,51 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 15:57:15 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2025/11/29 15:57:49 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2025/11/29 21:01:02 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	get_len(int n)
+{
+	long	nb;
+	size_t	len;
+
+	nb = n;
+	len = (nb <= 0);
+	while (nb != 0)
+	{
+		nb /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	long	nb;
+	size_t	len;
+	char	*str;
+
+	len = get_len(n);
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	nb = n;
+	if (nb == 0)
+		str[0] = '0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (nb > 0)
+	{
+		len--;
+		str[len] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	return (str);
+}
