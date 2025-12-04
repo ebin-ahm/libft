cda99d743298f98c2e9bfa4dcbb3db1cbcccdae8
Edieusdi Bin Ahmad <ebin-ahm@c2r2s7.42singapore.sg>
2025-12-04 23:17:02 +0800

    update

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 18:23:58 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2025/12/04 18:26:05 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2025/12/04 23:16:15 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_list;
+	t_list	*new_node;
+	void	*new_content;
+
+	new_list = NULL;
+	while (lst)
+	{
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
+		if (!new_node)
+		{
+			del(new_content);
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
